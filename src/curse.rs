6bc8dbaab6d84ebe156bc8c54f90da9d198e8995
dablenparty <hdavenport8902@gmail.com>
2022-12-16 16:09:18 -0500

    Implemented CurseFile::get_info

--- src/curse.rs
+++ src/curse.rs
@@ -1,3 +1,4 @@
+use reqwest::Client;
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Copy, Deserialize)]
@@ -7,6 +8,28 @@ struct CurseFile {
     #[serde(rename = "projectID")]
     project_id: u32,
     required: bool,
+}
+
+impl CurseFile {
+    async fn get_info(
+        &self,
+        client: &Client,
+        api_key: &str,
+    ) -> crate::error::Result<serde_json::Value> {
+        const BASE_CURSE_URL: &str = "https://api.curseforge.com";
+        let endpoint = format!("/v1/mods/{}/files/{}", self.project_id, self.file_id);
+        let url = format!("{}{}", BASE_CURSE_URL, endpoint);
+        let response = client
+            .get(&url)
+            .header("Accept", "application/json")
+            .header("x-api-key", api_key)
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<serde_json::Value>()
+            .await?;
+        Ok(response)
+    }
 }
 
 #[derive(Debug, Clone, Deserialize)]
