d16de179525eafa497367d70f06bb6e2d8b567cb
dablenparty <hdavenport8902@gmail.com>
2022-12-16 16:08:55 -0500

    Created custom error enum

---
+++ src/error.rs
@@ -0,0 +1,33 @@
+use std::{error, fmt, io};
+
+#[allow(clippy::enum_variant_names)]
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    ReqwestError(reqwest::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IoError(err) => write!(f, "IO error: {}", err),
+            Error::ReqwestError(err) => write!(f, "Reqwest error: {}", err),
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::IoError(err)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::ReqwestError(err)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod error;
 mod curse;
 
 fn main() {
