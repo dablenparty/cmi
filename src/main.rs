7ed070513c9f936e80215a2bba6291dc1eaedad6
dablenparty <hdavenport8902@gmail.com>
2022-12-20 14:42:26 -0500

    Implemented clap

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(clippy::module_name_repetitions)]
 
-use std::path::Path;
+use std::path::PathBuf;
 
+use clap::Parser;
 use curse::CurseModpack;
 
 mod curse;
 mod error;
 mod util;
 
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct CommandLineArgs {
+    /// The zip file to install from
+    #[clap(required = true)]
+    modpack_zip: PathBuf,
+    /// The target directory to install to
+    #[arg(required = true)]
+    target: PathBuf,
+}
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().expect("Failed to load .env");
-    // TODO: clap
-    let target = std::env::args().nth(1).expect("target not specified");
-    let zip = std::env::args().nth(2).expect("zip not specified");
 
-    let target = Path::new(&target);
-    let zip = Path::new(&zip);
+    let args = CommandLineArgs::parse();
 
-    let mut modpack = CurseModpack::load(zip).await.unwrap();
-    modpack.install_to(target).await.unwrap();
+    let mut modpack = CurseModpack::load(&args.modpack_zip).await.unwrap();
+    modpack.install_to(&args.target).await.unwrap();
 }
