ff4b7e74e7c43157e2e558fe6ad0b645ed220b75
dablenparty <hdavenport8902@gmail.com>
2022-12-22 19:56:26 -0500

    Fixed documentation in util

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 use std::path::{Component, Path};
 
 /// Function taken from the [zip](https://docs.rs/zip/0.6.3/src/zip/read.rs.html#896-911) crate source.
-/// This function is used to sanitize the file name of a zip entry.F
+/// This is used to sanitize the file name of a zip entry.
 pub fn sanitize_zip_filename(filename: &str) -> Option<&Path> {
     if filename.contains('\0') {
         return None;
